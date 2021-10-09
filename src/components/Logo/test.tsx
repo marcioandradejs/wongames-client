f8f467a7145ee054a5975ec5bc8ccceca201d080
marcioandrade <marcioandrade.dev@gmail.com>
2021-10-09 00:37:38 -0300

    Criando os primeiros testes

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,11 +1,24 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
+// renderizar o componente `render`
+// selecionar o elemento a ser testado `screen` (queries) - getBylabel...
+// expect - assertion - asserção - comparação - análise (Espero que renderize a logo branca)
+
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
-    // renderizar o componente `render`
-    // selecionar o elemento a ser testado `screen` (queries) - getBylabel...
-    // expect - assertion - asserção - comparação - análise (Espero que renderize a logo branca)
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
