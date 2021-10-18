80fffdafd66525ec1c4ed0b93645f5951c3a5e3d
marcioandrade <marcioandrade.dev@gmail.com>
2021-10-18 01:41:28 -0300

    criando o footer (faltam os testes)

---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+
+import Footer from '.'
+
+describe('<Footer />', () => {
+  it('should render 4 columns topics', () => {
+    // Contact
+    // Follow us
+    // links
+    // location
+  })
+})
