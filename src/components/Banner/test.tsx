637d8a2488af53e11cb3bd8cb0444a07db25ceab
marcioandrade <marcioandrade.dev@gmail.com>
2021-10-18 14:50:52 -0300

    testes iniciais

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -3,11 +3,31 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    renderWithTheme(<Banner />)
-    // verificar se o title existe
-    // verificar se o subtitle existe
-    // verificar se o image existe
+    renderWithTheme(<Banner {...props} />)
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Play the new CrashLands season/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('img', {
+        name: /Defy death/i
+      })
+    ).toBeInTheDocument()
   })
 })
