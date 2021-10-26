776453e26dd6015fc0092851ae43cc43d6a492b0
marcioandrade <marcioandrade.dev@gmail.com>
2021-10-25 22:33:31 -0300

    Criando template para a Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,15 +9,12 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Won Games</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, NextJS, Styled Components, Jest, React Testing Library, Storybook, Eslint, Prettier and PlopJS Generators"
-        />
+        <meta name="description" content="The best Game Stores in the world" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
