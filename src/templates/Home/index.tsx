cae93e38b6085444e1ead8bea6cb7be9e80a8fca
marcioandrade <marcioandrade.dev@gmail.com>
2021-10-26 01:20:10 -0300

    Home - criando mocks de dados

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,3 +1,9 @@
+import { BannerProps } from 'components/Banner'
+import { GameCardProps } from 'components/GameCard'
+import Highlight, { HighlightProps } from 'components/Highlight'
+import BannerSlider from 'components/BannerSlider'
+import GameCardSlider from 'components/GameCardSlider'
+
 import { Container } from 'components/Container'
 import Footer from 'components/Footer'
 import Heading from 'components/Heading'
@@ -5,34 +11,68 @@ import Menu from 'components/Menu'
 
 import * as S from './styles'
 
-const Home = () => (
+export type HomeTemplateProps = {
+  banners: BannerProps[]
+  newGames: GameCardProps[]
+  mostPopularHighlight: HighlightProps
+  mostPopularGames: GameCardProps[]
+  upcommingGames: GameCardProps[]
+  upcommingHighligth: HighlightProps
+  upcommingMoreGames: GameCardProps[]
+  freeGames: GameCardProps[]
+  freeHighligth: HighlightProps
+}
+
+const Home = ({
+  banners,
+  newGames,
+  mostPopularHighlight,
+  mostPopularGames,
+  upcommingGames,
+  upcommingHighligth,
+  upcommingMoreGames,
+  freeGames,
+  freeHighligth
+}: HomeTemplateProps) => (
   <section>
     <Container>
       <Menu />
+      <BannerSlider items={banners} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary" color="black">
         News
       </Heading>
+      <GameCardSlider items={newGames} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary">
         Most popular
       </Heading>
+
+      <Highlight {...mostPopularHighlight} />
+      <GameCardSlider items={mostPopularGames} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary">
         Upcomming
       </Heading>
+
+      <GameCardSlider items={upcommingGames} />
+      <Highlight {...upcommingHighligth} />
+      <GameCardSlider items={upcommingMoreGames} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary">
         Free Games
       </Heading>
+
+      <Highlight {...freeHighligth} />
+      <GameCardSlider items={freeGames} />
     </Container>
 
     <Container>
