fbf50f8159ffa3c5b5e95f44c1b806f196cd8c71
marcioandrade <marcioandrade.dev@gmail.com>
2021-10-28 15:20:38 -0300

    TextFild - adicionando msg de erro

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -10,6 +10,7 @@ export type TextFieldProps = {
   icon?: React.ReactNode
   iconPosition?: 'left' | 'right'
   disabled?: boolean
+  error?: string
 } & InputHTMLAttributes<HTMLInputElement>
 
 const TextField = ({
@@ -19,6 +20,7 @@ const TextField = ({
   labelFor = '',
   initialValue = '',
   onInput,
+  error,
   disabled = false,
   ...props
 }: TextFieldProps) => {
@@ -32,7 +34,7 @@ const TextField = ({
   }
 
   return (
-    <S.Wrapper disabled={disabled}>
+    <S.Wrapper disabled={disabled} error={!!error}>
       {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
       <S.InputWrapper>
         {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
@@ -45,6 +47,7 @@ const TextField = ({
           {...props}
         />
       </S.InputWrapper>
+      {!!error && <S.Error>{error}</S.Error>}
     </S.Wrapper>
   )
 }
--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -85,6 +85,21 @@ describe('<TextField />', () => {
     expect(onInput).not.toHaveBeenCalled()
   })
 
+  it('should render with error', () => {
+    const { container } = renderWithTheme(
+      <TextField
+        icon={<Email data-testid="icon" />}
+        label="TextField"
+        labelFor="TextField"
+        error="Error message"
+      />
+    )
+
+    expect(screen.getByText('Error message')).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
   it('Is accessible by tab', () => {
     renderWithTheme(
       <TextField label="TextField" labelFor="TextField" id="TextField" />
