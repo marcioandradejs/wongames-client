300b0c922028c58ae8129f07cb0ea97c26577077
marcioandrade <marcioandrade.dev@gmail.com>
2021-10-28 17:58:20 -0300

    criando testes para o template

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -11,10 +11,31 @@ describe('<Auth />', () => {
       </Auth>
     )
 
+    // verifique se tem 2 logos
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    // verifica se tem heading principal do banner
     expect(
       screen.getByRole('heading', {
         name: /All your favorite games in one place/i
       })
     ).toBeInTheDocument()
+
+    // verifica se tem subtitulo
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform/i
+      })
+    ).toBeInTheDocument()
+
+    // verifica se tem o title do content
+    expect(
+      screen.getByRole('heading', {
+        name: /Auth title/i
+      })
+    ).toBeInTheDocument()
+
+    // verifica se o children tá sendo renderizado
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
