19a2af2bb447e814b4e11399201a91367f0c239c
marcioandrade <marcioandrade.dev@gmail.com>
2021-10-28 17:46:07 -0300

    Criando estrutura inicial do template Auth

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,13 +1,20 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Auth />)
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>
+    )
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
   })
 })
