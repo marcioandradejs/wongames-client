89354966fe5a59761aa7249cb69dcd551020cc29
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-04 23:58:26 -0300

    Criando o slider das thumbnails

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,64 @@
+import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
+import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
+
+import Slider, { SliderSettings } from 'components/Slider'
+
+import * as S from './styles'
+
+const settings: SliderSettings = {
+  nextArrow: <ArrowRight aria-label="Next image" />,
+  prevArrow: <ArrowLeft aria-label="Previous image" />,
+  arrows: true,
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    }
+  ]
+}
+
+export type GalleryImageProps = {
+  src: string
+  label: string
+}
+
+export type GalleryProps = {
+  items: GalleryImageProps[]
+}
+
+const Gallery = ({ items }: GalleryProps) => (
+  <S.Wrapper>
+    <Slider settings={settings}>
+      {items.map((item, index) => (
+        <img
+          role="button"
+          key={`thumb-${index}`}
+          src={item.src}
+          alt={`Thumb - ${item.label}`}
+        />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
+
+export default Gallery
