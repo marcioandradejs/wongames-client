c900b1a8d8893ad55f81fda8c6976224b5e61281
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-07 20:44:57 -0300

    Adicionando o GameDetails

--- src/templates/Game/styles.ts
+++ src/templates/Game/styles.ts
@@ -50,7 +50,7 @@ export const SectionGallery = styled(Section)`
   display: none;
 
   ${media.greaterThan('medium')`
-    display: block;
+  display: block;
   `}
 `
 
@@ -60,6 +60,19 @@ export const SectionDescription = styled(Section)`
       color: ${theme.colors.gray};
       font-size: ${theme.font.sizes.xsmall};
       margin-top: ${theme.spacings.medium};
+    }
+  `}
+`
+
+export const SectionGameDetails = styled(Section)`
+  ${({ theme }) => css`
+    > div {
+      padding-bottom: ${theme.spacings.xlarge};
+      border-bottom: 0.1rem solid rgba(181, 181, 81, 0.3);
+
+      ${media.greaterThan('medium')`
+        padding-bottom: calc(${theme.spacings.xxlarge} * 2);
+      `}
     }
   `}
 `
