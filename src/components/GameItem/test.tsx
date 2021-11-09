6235f7116419cb0172dc92250d8d5abb20cea1ca
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-09 11:51:18 -0300

    adicionando estilos

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -13,19 +13,15 @@ describe('<GameItem />', () => {
   it('should render the item', () => {
     renderWithTheme(<GameItem {...props} />)
 
-    // verificar o title se foi renderizado
     expect(
       screen.getByRole('heading', { name: /Red Dead Redemption 2/i })
     ).toBeInTheDocument()
 
-    // verificar a imagem
-    // expect(screen.getByAltText(/Red Dead Redemption 2/i)).toBeInTheDocument()
     expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
       'src',
       props.img
     )
 
-    // verificar o preco
     expect(screen.getByText(/R\$ 215,00/i)).toBeInTheDocument()
   })
 })
