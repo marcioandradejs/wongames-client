bc92ae1a4b224a3b097dae427b007511d733bc87
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-10 20:05:19 -0300

    Criando OrdersList - estrutura, estilos e testes

---
+++ src/components/OrdersList/stories.tsx
@@ -0,0 +1,18 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import OrdersList, { OrdersListProps } from '.'
+
+import mock from './mock'
+
+export default {
+  title: 'Profile/OrdersList',
+  component: OrdersList,
+  args: {
+    items: mock
+  }
+} as Meta
+
+export const Default: Story<OrdersListProps> = (args) => (
+  <div style={{ maxWidth: 850, margin: 'auto' }}>
+    <OrdersList {...args} />
+  </div>
+)
