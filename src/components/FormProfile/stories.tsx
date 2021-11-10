3c965a703c2eafc03e0fd2b813dc7b1b34143156
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-10 17:29:09 -0300

    Criando estrutura com TDD

---
+++ src/components/FormProfile/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import FormProfile from '.'
+
+export default {
+  title: 'form/FormProfile',
+  component: FormProfile
+} as Meta
+
+export const Default: Story = () => <FormProfile />
