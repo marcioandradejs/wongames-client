91a9e425ccebbc7267f553ad62b4d65e216380a2
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-17 19:28:36 -0300

    Criando ApolloProvider e usando o useQuery hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,4 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from '@apollo/client'
+import { useQuery, gql } from '@apollo/client'
 
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
@@ -12,25 +6,24 @@ import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
-  const client = new ApolloClient({
-    uri: 'http://localhost:1337/graphql',
-    cache: new InMemoryCache()
-  })
-
-  client.query({
-    query: gql`
-      query getGames {
-        games {
-          name
-        }
+  const { data, loading, error } = useQuery(gql`
+    query getGames {
+      games {
+        name
       }
-    `
-  })
+    }
+  `)
+
+  if (loading) return <p>Loading...</p>
+
+  if (error) return <p>{error}</p>
+
+  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
 
   return <Home {...props} />
 }
 
-export function getServerSideProps() {
+export async function getServerSideProps() {
   return {
     props: {
       banners: bannersMock,
