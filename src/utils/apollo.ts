b6545e3cc08ee50fe2f1800b7dbfd5276ee18982
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-17 20:18:14 -0300

    Fix Apollo initialState e Cache

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -6,7 +6,7 @@ import {
 } from '@apollo/client'
 import { useMemo } from 'react'
 
-let apolloClient: ApolloClient<NormalizedCacheObject>
+let apolloClient: ApolloClient<NormalizedCacheObject | null>
 
 function createApolloClient() {
   return new ApolloClient({
@@ -16,7 +16,7 @@ function createApolloClient() {
   })
 }
 
-export function initializeApollo(initialState = {}) {
+export function initializeApollo(initialState = null) {
   // serve para verificar se já existe uma instância, para não criar outra
   const apolloClientGlobal = apolloClient ?? createApolloClient()
 
@@ -32,7 +32,7 @@ export function initializeApollo(initialState = {}) {
   return apolloClient
 }
 
-export function useApollo(initialState = {}) {
+export function useApollo(initialState = null) {
   const store = useMemo(() => initializeApollo(initialState), [initialState])
   return store
 }
