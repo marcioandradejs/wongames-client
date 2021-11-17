08600f62adc7e77591c1e64277df1d7ea442df1a
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-17 14:16:27 -0300

    adicionando testes

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -1,14 +1,27 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import UserDropdown from '.'
 
 describe('<UserDropdown />', () => {
   it('should render the username', () => {
-    renderWithTheme(<UserDropdown />)
+    renderWithTheme(<UserDropdown username="Marcio" />)
+
+    expect(screen.getByText(/Marcio/i)).toBeInTheDocument()
   })
 
   it('should render the menu', () => {
-    renderWithTheme(<UserDropdown />)
+    renderWithTheme(<UserDropdown username="Marcio" />)
+
+    // open menu
+    userEvent.click(screen.getByText(/Marcio/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
   })
 })
