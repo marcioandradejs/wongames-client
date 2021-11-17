4fa18e7c64d4134ffb20c396b39aff4643043cc9
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-17 13:34:03 -0300

    CartList - criando opcao de mostrar carrinho vazio

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -21,4 +21,11 @@ describe('<CartList />', () => {
 
     expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
   })
+
+  it('should render the button', () => {
+    renderWithTheme(<CartList />)
+
+    expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument()
+    expect(screen.queryByText(/total/i)).not.toBeInTheDocument()
+  })
 })
