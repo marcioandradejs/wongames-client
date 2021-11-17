9c989422dac2f2aa682ec1da1233c4f8c13c8cfb
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-17 14:06:56 -0300

    UserDropdown Parte 1: criando o componente e estilos

---
+++ src/components/UserDropdown/stories.tsx
@@ -0,0 +1,22 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import UserDropdown, { userDropdownProps } from '.'
+
+export default {
+  title: 'UserDropdown',
+  component: UserDropdown,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<userDropdownProps> = (args) => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <UserDropdown {...args} />
+  </div>
+)
+
+Default.args = {
+  username: 'Marcio'
+}
---
+++ src/components/UserDropdown/test.tsx
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import UserDropdown from '.'
+
+describe('<UserDropdown />', () => {
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown />)
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown />)
+  })
+})
