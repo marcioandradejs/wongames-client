0fdb3d0106f541b0322a5ccb6833191f5c9b945b
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-17 19:53:12 -0300

    Criando a configuração do Apollo para trabalhar com SSR/SSG no NextJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,17 @@ export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
+// ATENÇÃO:
+// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
+
+// getStaticProps => gerar estático em build time (gatsby)
+// getServerSideProps => gerar via ssr a cada request (Nunca vai para o bundle do client)
+// getInitialProps => gerar via ssr a cada request (Vai para o client, faz gydrate do lado do client depois do primeiro request)
 export async function getServerSideProps() {
+  // faz lógica
+  // pode ser buscar dados numa API
+  // fazer calculo|leitura de context
+  // retorno dos dados
   return {
     props: {
       banners: bannersMock,
