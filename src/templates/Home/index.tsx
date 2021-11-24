155d9c55348b917d4247c8ded5811c4e9e02059c
marcioandrade <marcioandrade.dev@gmail.com>
2021-11-23 22:52:39 -0300

    Adicionando opção para cor da seta no Showcase

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -38,7 +38,7 @@ const Home = ({
       </S.SectionBanner>
     </Container>
     <S.SectionNews>
-      <Showcase title="News" games={newGames} />
+      <Showcase title="New Games" games={newGames} color="black" />
     </S.SectionNews>
     <Showcase
       title="Most Popular"
