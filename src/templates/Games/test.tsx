04531309933126a6458cf8adc4e72566f2b0e7ca
marcioandrade <marcioandrade.dev@gmail.com>
2021-12-06 23:31:36 -0300

    Testando o Apollo com MockedProvider

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -1,9 +1,11 @@
 import { screen } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
+
 import { renderWithTheme } from 'utils/tests/helpers'
-import gamesMock from 'components/GameCardSlider/mock'
 import filterItemsMock from 'components/ExploreSidebar/mock'
 
 import Games from '.'
+import { QUERY_GAMES } from 'graphql/queries/games'
 
 jest.mock('templates/Base', () => ({
   __esModule: true,
@@ -19,24 +21,66 @@ jest.mock('components/ExploreSidebar', () => ({
   }
 }))
 
-jest.mock('components/GameCard', () => ({
-  __esModule: true,
-  default: function Mock() {
-    return <div data-testid="Mock GameCard" />
-  }
-}))
-
 describe('<Games />', () => {
   it('should render sections', () => {
     renderWithTheme(
-      <Games filterItems={filterItemsMock} games={[gamesMock[0]]} />
+      <MockedProvider mocks={[]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
     )
 
-    expect(screen.getByTestId('Mock ExploreSidebar')).toBeInTheDocument()
-    expect(screen.getByTestId('Mock GameCard')).toBeInTheDocument()
+    expect(screen.getByText(/loading.../i)).toBeInTheDocument()
+  })
+
+  it('should render loading when starting the template', async () => {
+    renderWithTheme(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: QUERY_GAMES,
+              variables: {
+                limit: 15 // Precisa ter exatamente o mesmo valor da index
+              }
+            },
+            result: {
+              data: {
+                games: [
+                  {
+                    name: 'Project Hospital',
+                    slug: 'project-hospital',
+                    cover: {
+                      url: '/uploads/project_hospital_9c8d00a655.jpg'
+                    },
+                    developers: [{ name: 'Oxymoron Games' }],
+                    price: 23.79,
+                    __typename: 'Game'
+                  }
+                ]
+              }
+            }
+          }
+        ]}
+        addTypename={false}
+      >
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    // ut starts wutgiyt data
+    // shows loading
+    expect(screen.getByText(/loading.../i)).toBeInTheDocument()
+
+    // we wait until we have data to get the alements
+    // get => Tem certeza do elemento
+    // query => Não tem o elemento
+    // find => Processos assincronos
+    expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
+
+    expect(await screen.findByText(/Project Hospital/i)).toBeInTheDocument()
 
     expect(
-      screen.getByRole('button', { name: /show more/i })
+      await screen.findByRole('button', { name: /show more/i })
     ).toBeInTheDocument()
   })
 })
