996c2f83e98cb5c3f5edd0806712b805c440b409
marcioandrade <marcioandrade.dev@gmail.com>
2021-12-08 17:14:18 -0300

    fix Menu test

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { screen, fireEvent } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
@@ -8,34 +8,34 @@ describe('<Menu />', () => {
     renderWithTheme(<Menu />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getAllByLabelText(/shopping cart/i)).toHaveLength(2)
   })
 
-  it('should render the open/close mobile menu', () => {
+  it('should handle the open/close mobile menu', () => {
     renderWithTheme(<Menu />)
 
-    // selecionar o nosso menuFull
+    // selecionar o nosso MenuFull
     const fullMenuElement = screen.getByRole('navigation', { hidden: true })
 
-    // verificar se o menu está escondido
+    // verificar se o menu tá escondido
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
 
-    // clicar no botao de abrir o menu e verificar se ele abriu
+    // clicar no botão de abrir o menu e verificar se ele abriu
     fireEvent.click(screen.getByLabelText(/open menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
     expect(fullMenuElement).toHaveStyle({ opacity: 1 })
 
-    // clicar no botao de fechar o menu e verificar se ele fechou
+    // clicar no botão de fechar o menu e verificar se ele fechou
     fireEvent.click(screen.getByLabelText(/close menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 
   it('should show register box when logged out', () => {
-    render(<Menu />)
+    renderWithTheme(<Menu />)
 
     expect(screen.queryByText(/my profile/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
@@ -46,9 +46,9 @@ describe('<Menu />', () => {
   it('should show wishlist and account when logged in', () => {
     renderWithTheme(<Menu username="Marcio" />)
 
-    expect(screen.getByText(/my account/i)).toBeInTheDocument()
-    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
-    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.getAllByText(/my profile/i)).toHaveLength(2)
+    expect(screen.getAllByText(/wishlist/i)).toHaveLength(2)
+    expect(screen.queryByText(/sign in/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
   })
 })
