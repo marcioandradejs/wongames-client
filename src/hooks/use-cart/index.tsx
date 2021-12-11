191730303f4a6b47f19694a98fae1d4c998c3193
marcioandrade <marcioandrade.dev@gmail.com>
2021-12-10 21:28:36 -0300

    criando testes para o hook customizado

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -1,21 +1,44 @@
-import { createContext, useContext } from 'react'
+import { useContext, createContext, useState, useEffect } from 'react'
+import { getStorageItem } from 'utils/localStorage'
 
-export type CartContextData = {}
+const CART_KEY = 'cartItems'
 
-export const CartContextDefaultValues = {}
+export type CartContextData = {
+  items: string[]
+}
+
+export const CartContextDefaultValues = {
+  items: []
+}
 
 export const CartContext = createContext<CartContextData>(
   CartContextDefaultValues
 )
-
 export type CartProviderProps = {
   children: React.ReactNode
 }
 
 const CartProvider = ({ children }: CartProviderProps) => {
-  return <CartContext.Provider value={{}}>{children}</CartContext.Provider>
+  const [cartItems, setCartItems] = useState<string[]>([])
+
+  useEffect(() => {
+    const data = getStorageItem(CART_KEY)
+
+    if (data) {
+      setCartItems(data)
+    }
+  }, [])
+
+  return (
+    <CartContext.Provider
+      value={{
+        items: cartItems
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
 }
 
 const useCart = () => useContext(CartContext)
-
 export { CartProvider, useCart }
