19c392f972772b13df764f9c2c221332146dfe7f
marcioandrade <marcioandrade.dev@gmail.com>
2021-12-13 18:22:42 -0300

    Fazendo processo simples de Sign Up no client

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -3,7 +3,7 @@ import { useState, InputHTMLAttributes } from 'react'
 import * as S from './styles'
 
 export type TextFieldProps = {
-  onInput?: (value: string) => void
+  onInputChange?: (value: string) => void
   label?: string
   initialValue?: string
   icon?: React.ReactNode
@@ -18,7 +18,7 @@ const TextField = ({
   label,
   name,
   initialValue = '',
-  onInput,
+  onInputChange,
   error,
   disabled = false,
   ...props
@@ -29,7 +29,7 @@ const TextField = ({
     const newValue = e.currentTarget.value
     setValue(newValue)
 
-    !!onInput && onInput(newValue)
+    !!onInputChange && onInputChange(newValue)
   }
 
   return (
