fe4ebd7088b164cfdcc9805d66f8a9f5cf6c4557
marcioandrade <marcioandrade.dev@gmail.com>
2022-02-03 12:31:10 -0300

    Ordenando jogos pelo preço

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -18,11 +18,29 @@ describe('Explore Page', () => {
     cy.getFields(priceFields)
     cy.getFields(platformFields)
     cy.getFields(genreFields)
-  })
+  });
 
   it('should show 15 games and show more games when show more is clicked', () => {
     cy.getByDataCy('game-card').should('have.length', 15)
     cy.findByRole('button', { name: /show more/i }).click()
     cy.getByDataCy('game-card').should('have.length', 30)
+  });
+
+  it('should order by price', () => {
+    cy.findByText(/lowest to highest/i).click()
+
+    cy.location('href').should('contain', 'sort=price%3Aasc')
+
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText('FREE').should('exist')
+    })
+
+    cy.findByText(/highest to lowest/i).click()
+
+    cy.location('href').should('contain', 'sort=price%3Adesc')
+
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText('FREE').should('not.exist')
+    })
   });
 })
