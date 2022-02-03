906cc87dbe98baab63dad4845b3c58e4f3025da4
marcioandrade <marcioandrade.dev@gmail.com>
2022-02-03 12:16:00 -0300

    Clicando para mostrar mais jogos na explore (show more)

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -3,9 +3,11 @@
 import { priceFields, platformFields, sortFields, genreFields } from '../../src/utils/filter/fields'
 
 describe('Explore Page', () => {
-  it('should render filters columns', () => {
+  before(() => {
     cy.visit('/games')
+  })
 
+  it('should render filters columns', () => {
     cy.findByRole('heading', {name: /sort by price/i}).should('exist')
     cy.findByRole('heading', {name: /^price/i}).should('exist')
     cy.findByRole('heading', {name: /platforms/i}).should('exist')
@@ -17,4 +19,10 @@ describe('Explore Page', () => {
     cy.getFields(platformFields)
     cy.getFields(genreFields)
   })
+
+  it('should show 15 games and show more games when show more is clicked', () => {
+    cy.getByDataCy('game-card').should('have.length', 15)
+    cy.findByRole('button', { name: /show more/i }).click()
+    cy.getByDataCy('game-card').should('have.length', 30)
+  });
 })
