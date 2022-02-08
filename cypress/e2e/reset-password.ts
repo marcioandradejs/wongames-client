476dec35f2239629473a6e0ba61d170489eded53
marcioandrade <marcioandrade.dev@gmail.com>
2022-02-08 15:45:36 -0300

    Utilizando o Intercept no Reset Password (fail cases)

---
+++ cypress/e2e/reset-password.ts
@@ -0,0 +1,41 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Reset Password', () => {
+  it('should show error if password does not match', () => {
+    cy.visit('/reset-password?code=12345')
+
+    cy.findAllByPlaceholderText(/^password/i).type('123')
+    cy.findAllByPlaceholderText(/confirm password/i).type('321')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+    cy.findByText(/confirm password does not match with password/i).should('exist')
+  });
+
+  it('should show error if code is not valid', () => {
+    cy.intercept('POST', '**/auth/reset-password', res => {
+      res.reply({
+        status: 400,
+        body: {
+          error: 'Bad Request',
+          message: [
+            {
+              messages: [
+                {
+                  message: 'Incorrect code provided'
+                }
+              ]
+            }
+          ]
+        }
+      })
+    })
+
+    cy.visit('/reset-password?code=wrong_code')
+
+    cy.findAllByPlaceholderText(/^password/i).type('1234')
+    cy.findAllByPlaceholderText(/confirm password/i).type('1234')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+    cy.findByText(/Incorrect code provided/i).should('exist')
+  });
+});
